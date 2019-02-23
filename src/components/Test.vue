10a7e4be57d5219bdf19ae62b3f47922cdd5eb7c
Jared Parkinson <jared@parkinson.im>
2019-02-22 22:47:32 -0700

    I now know how to reuse components

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="test">test</button>
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class Test extends Vue {
+  public test() {
+    alert('asdofij');
+  }
+}
+Vue.component('test-test', Test);
+</script>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
+    <test-test></test-test>
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue';
+import Test from '@/components/Test.vue';
+export default Vue.extend({});
+</script>
